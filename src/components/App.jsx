0ff08a23a43c92301185cbdd33cf2329bc1d42af
Streltsov Valerii <st.valera04@gmail.com>
2023-05-23 22:46:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import { ContactList } from './contactList/ContactList';
 import { Filter } from './filter/Filter';
 
 export const App = () => {
-  console.log('проверка');
   return (
     <>
       <h2>Phonebook</h2>
